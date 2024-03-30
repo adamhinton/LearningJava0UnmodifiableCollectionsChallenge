d9070e4e1444ab30d22356ec5c265216a16b747c
adamd <adam.d.hinton@gmail.com>
2024-03-30 00:20:17 -0400

    project specs notes

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -12,6 +12,55 @@ public class Main {
 }
 
 
+// Transaction class
+    // int routingNumber
+    // int customerID
+    // long transactionID
+    // int transactionAmount
+// More Txn details
+    // getters/setters for all fields
+    // constructor that takes all fields for ease of use
+
+// modify BA class
+    // make balance mutable
+    // include Transaction Collection
+        // Map<Long, Transaction> transactions
+    // getTransactions: Map<Long,Transaction>
+    // commitTransaction(int routingNumber, long transactionId, String customerID, double amount)
+
+// Modify BC
+    // getCustomerID:String; customerID is 15 digit string with leading zeros
+    // Code in other packages can't instantiate a new BC
+    // getAccounts: List<BankAccount>
+        // Return defensive copy of accounts
+        // multipe accts
+    // getAccount(AccountType type): Account
+        // single acct
+        // assume cr will have exactly one checking and one savings
+
+// Bank class
+    // Can look up cr by customerID
+    // routingNumber: int
+    // lastTransactionID: long
+    // Map<String, BankCustomer> customers
+    // fns:
+        // getCustomer(String id): BankCustomer
+        // addCustomer (String name, double checkingInitialDeposit, double savingsInitialDeposit)
+        // doTransaction (String id, AccountType type, double amount)
+            // I think the id is the customer's ID
+            // neg is withdrawal, pos is deposit
+            // Dont let balance go below zero
+
+// MAIN
+    // make bank, add cr
+    // let client get a BC instance by cr id, and review txns from single selected acct.
+        // txns should be UNMODIFIABLE. No side effects.
+
+// NOTES:
+    // ONLY Bank instance can withdraw/deposit, by passing customerID, acct type and amount
+    // So, mn shouldn't have access to the commit transaction code on BA itself.
+
+
 class BankAccount {
 
     public enum AccountType {CHECKING, SAVINGS}
